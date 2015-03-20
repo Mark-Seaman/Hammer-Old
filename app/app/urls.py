df04420637ada4d5f18e4868647ebba0d31cfba9
Mark Seaman <seaman@seaman-hammer>
2015-03-20 17:07:43 -0600

    Create a simple HTTP response with a hard coded answer.

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,13 +1,22 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from django.http import HttpResponse
+
 admin.autodiscover()
 
+# urlpatterns = patterns('',
+#     # Examples:
+#     # url(r'^$', 'app.views.home', name='home'),
+#     # url(r'^blog/', include('blog.urls')),
+#
+#     url(r'^admin/', include(admin.site.urls)),
+# )
+
+def hello(request):
+    return HttpResponse('<h1>Hello-World</h1>'+\
+                        '<p>This app has a hard-coded templates</p>')
+
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'app.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', hello),
 )
-
