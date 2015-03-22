15667d5c13489d0158e716c3c900583761e40f34
Mark Seaman <seaman@seaman-hammer>
2015-03-22 14:33:52 -0600

    Build the REPL logic with help text

--- bin/jack_script.py
+++ bin/jack_script.py
@@ -1,13 +1,37 @@
+# Run the test for Jack Script
+def jack_script_test():
+    print ('jack_script_test()')
+
+
+# Run the test for Jack Script
+def jack_script_help(command):
+    print ('Unknown command:  %s'%command)
+    help_text = '''
+        Jack Script Commands:
+            test -- run the requested tests
+    '''
+    print (help_text)
+
+
+# Read and do a single command
+def read_and_exec(command):
+    if not command or command=='exit':
+        return False
+    if command=='test':
+        jack_script_test()
+        return True
+    jack_script_help(command)
+    return True
+
 
 # REPL for Jack Script
-def jack_script():
+def jack_script_loop():
     print ('Jack Hammer Script Commander:')
-    while True:
-        s = input('>')
-        if not s or s=='exit':
-            print ('Good bye')
-            exit(0)
-        print ('s = %s'%s)
+    while read_and_exec(input(' ? ')):
+        pass
+    print ('Jack Script done')
 
 
-jack_script()
+# Run as a stand-alone script
+if __name__=='__main__':
+    jack_script_loop()
