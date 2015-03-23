36a6d4b04b1f8ccce54d9fcb230f5af5601f89c0
Mark Seaman <seaman@seaman-hammer>
2015-03-23 08:14:39 -0600

    Create description within the jack_script.py file

--- bin/jack_script.py
+++ bin/jack_script.py
@@ -1,3 +1,22 @@
+'''
+Jack Script
+
+Command interpreter for Jack Hammer scripting language.
+
+Functional requirements:
+
+ * Run from the Linux shell
+ * Each line of stdin is a script command
+ * Blank line or 'exit' ends to the program
+ * Illegal command shows the help text
+ * Jack Script has a self test 
+ * Description is contained in the source code
+
+Next steps:
+
+'''
+
+
 # Run the test for Jack Script
 def jack_script_test():
     print ('jack_script_test()')
