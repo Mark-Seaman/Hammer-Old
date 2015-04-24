5c9285cb1abca0d64a3eaf72649315939955c1cc
Mark Seaman <seaman@seaman-hammer>
2015-04-24 09:56:16 -0600

    comment out tests

--- bin/doc_test.py
+++ bin/doc_test.py
@@ -9,7 +9,7 @@ Run all of the tests that are available.  Output the test results.
 from os import listdir, environ, system
 from os.path import join
 
-from formatter import map_muse_to_markdown, map_markdown_to_muse
+#from formatter import map_muse_to_markdown, map_markdown_to_muse
 
 
 def count_lines(filepath, min=None, max=None):
@@ -27,9 +27,9 @@ def nose_test():
 	assert(True)
 
 
-def assemble_doc_test():
-	system('bo assemble > /dev/null')
-	count_lines('docs/doc/doc.md', 870, 900)
+# def assemble_doc_test():
+# 	system('bo assemble > /dev/null')
+# 	count_lines('docs/doc/doc.md', 870, 900)
 
 
 def doc_file_test():
@@ -43,62 +43,60 @@ def bin_file_test():
 	assert (14==num_files)
 
 
-def markdown_convert_test():
-	md = '''
-* Heading 1
-** Heading 2
-**Bold** this is plain
- * List item
- my link [[http://Shrinking-World.org][Shrinking World Solutions]] more text
- my link [[http://Shrinking-World.org]] more text
-'''.split('\n')
+# def markdown_convert_test():
+# 	md = '''
+# * Heading 1
+# ** Heading 2
+# **Bold** this is plain
+#  * List item
+#  my link [[http://Shrinking-World.org][Shrinking World Solutions]] more text
+#  my link [[http://Shrinking-World.org]] more text
+# '''.split('\n')
 
-	muse = '''
-# Heading 1
-## Heading 2
-**Bold** this is plain
-* List item
- my link [Shrinking World Solutions](http://Shrinking-World.org.md) more text
- my link [http://Shrinking-World.org](http://Shrinking-World.org.md) more text
-'''
+# 	muse = '''
+# # Heading 1
+# ## Heading 2
+# **Bold** this is plain
+# * List item
+#  my link [Shrinking World Solutions](http://Shrinking-World.org.md) more text
+#  my link [http://Shrinking-World.org](http://Shrinking-World.org.md) more text
+# '''
 
-	answer = '\n'.join([map_muse_to_markdown(x) for x in md])
-	print ('answer: ',answer)
-	print ('expected: ',muse)
-	assert(answer==muse)
+# 	answer = '\n'.join([map_muse_to_markdown(x) for x in md])
+# 	print ('answer: ',answer)
+# 	print ('expected: ',muse)
+# 	assert(answer==muse)
 
 
-def muse_convert_test():
-	muse = '''
-# Heading 1
-## Heading 2
-**Bold** this is plain
-* List item
- my link [Shrinking World Solutions](http://Shrinking-World.org) more text
- my link [[Shrinking World Solutions]] more text
-'''.split('\n')
+# def muse_convert_test():
+# 	muse = '''
+# # Heading 1
+# ## Heading 2
+# **Bold** this is plain
+# * List item
+#  my link [Shrinking World Solutions](http://Shrinking-World.org) more text
+#  my link [[Shrinking World Solutions]] more text
+# '''.split('\n')
 
-	md = '''
-* Heading 1
-** Heading 2
-**Bold** this is plain
- * List item
- my link [[http://Shrinking-World.org][Shrinking World Solutions]] more text
- my link [[Shrinking World Solutions]] more text
-'''
+# 	md = '''
+# * Heading 1
+# ** Heading 2
+# **Bold** this is plain
+#  * List item
+#  my link [[http://Shrinking-World.org][Shrinking World Solutions]] more text
+#  my link [[Shrinking World Solutions]] more text
+# '''
 
-	answer = '\n'.join([map_markdown_to_muse(x) for x in muse])
-	print ('answer: ',answer)
-	print ('expected: ',md)
-	assert(answer==md)
+# 	answer = '\n'.join([map_markdown_to_muse(x) for x in muse])
+# 	print ('answer: ',answer)
+# 	print ('expected: ',md)
+# 	assert(answer==md)
 
 
 def command_test():
 	system('''{ 
-		bo files
-		bo help
-		bo list
-		bo script 
+		doc files
+		doc help
 		} > /tmp/doc-test''')
 	count_lines('/tmp/doc-test', min=80, max=90)
 	
