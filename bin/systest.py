299fe91320e812cfe8f4bc4eace7ee770671c83e
Mark Seaman <seaman@seaman-hammer>
2015-05-08 14:51:58 -0600

    add bin files to project

---
+++ bin/systest.py
@@ -0,0 +1,56 @@
+from os import system
+from re import sub
+from sys import argv
+
+from cmd_test import cmd_checker
+from doc_test import doc_checker
+from vc_test import vc_checker
+from tst_test import tst_checker
+from tst import run_diff_checks, shell, lines, limit_lines
+
+
+def cmd_python_pip_test():
+    '''Check the python setup for pip'''
+    return shell('pip list')
+
+
+def nose_test_execution():
+    '''Run all nose tests  and report any changes'''
+    system('nose 2> /tmp/nose; sleep 1')
+    return sub(r'\.\d\d\ds', r'.xxxs', shell('cat /tmp/nose'))
+
+
+def process_status_test():
+    '''Make sure that there are not too many processes running'''
+    return limit_lines ('ps -e',190,200)
+
+
+def pwd_test():
+    '''List the current directory   '''
+    return shell('pwd')
+
+
+def system_test_list():
+    '''Create a list of tests to manage'''
+    return {
+        'sys-nose': nose_test_execution,
+        'sys-vc': vc_checker,
+        'sys-cmd': cmd_checker,
+        'sys-doc': doc_checker,
+        'sys-tst': tst_checker,
+        'sys-pip': cmd_python_pip_test,
+        'sys-ps': process_status_test,
+        'sys-pwd': pwd_test,
+    }
+
+
+def system_checker():
+    '''Execute all tests for the tst command'''
+    my_tests = system_test_list()
+    run_diff_checks('system', my_tests)
+
+
+# Create a script that can be run from the shell
+if __name__=='__main__':
+    system_checker()
+    
---
+++ bin/tst_test.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+from glob import glob
+from os import system
+from os.path import join
+from subprocess import Popen,PIPE
+from sys import argv
+from re import sub
+
+from tst import run_diff_checks, shell, lines, limit_lines
+
+
+def ls_test():
+    '''   List files in the current directory   '''
+    return shell('ls')
+
+
+def lines_test():
+    '''   Count the lines of source code in the tst.py file   '''
+    return limit_lines ('cat bin/tst.py',200,300)
+
+
+def like_test():
+    '''   Test the like switch to approve the correct answer   '''
+    system('''
+        echo do not like this > test/dummy.correct
+        tst like dummy > /dev/null
+        ''')
+    return ''
+
+ 
+def list_tests():
+    '''Make a list of all tests to run'''
+    return shell('tst list')
+
+
+def output_test():
+    '''   Display the output from the last test run   '''
+    return shell('tst output cmd_add')
+
+
+def help_test():
+    return shell('tst help')
+
+
+def tst_checker():
+    '''Execute all tests for the tst command'''
+    my_tests = {
+        'tst-like': like_test,
+        'tst-lines': lines_test,
+        'tst-ls': ls_test,
+        'tst-list': list_tests,
+        'tst-help': help_test,
+        'tst-output': output_test,
+    }
+    run_diff_checks('tst', my_tests)
+
+   
+
+# Create a script that can be run from the tst
+if __name__=='__main__':
+    tst_checker()
