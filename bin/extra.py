f47ab8988cae608737b1a911442d462299c1509d
Mark Seaman <markseaman@MacBook>
2015-06-17 12:01:50 -0600

    Automatic commit of Hammer changes

--- bin/extra.py
+++ bin/extra.py
@@ -25,7 +25,11 @@ def extra_outline(topic):
     path = join(join(environ['book'],'chapters'), topic+'.md') 
     outline = convert_to_outline(extract_headings(open(path).read()))
     print(outline)
-   
+ 
+ 
+def extra_remove(filename):
+	system ('rm $book/extra/'+filename+'.md')
+
 
 def extra_command(argv):
 	if len(argv)>1:
@@ -33,6 +37,8 @@ def extra_command(argv):
 			extra_edit(argv[2:])
 		elif argv[1]=='outline':
 			extra_outline(argv[2:])
+		elif argv[1]=='rm':
+			extra_remove(argv[2])
 	else:
 		extra_show(argv[1:])
 
