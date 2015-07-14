518bb1abdc28b7458509321059267e4c019a8230
Mark Seaman <seaman@macmini>
2015-07-14 16:00:03 -0600

    Hourly Hammer commit

--- bin/store.py
+++ bin/store.py
@@ -9,6 +9,7 @@ store = Redis("localhost")
 # Save the item for later
 def save(key,value):
     store.set(key,value)
+    return key
 
 
 # Recall a previously stored item
@@ -26,3 +27,18 @@ def expire(key,seconds):
 #  Check the expiration time
 def expiration(key):
     return store.ttl(key)
+
+
+def store_command(argv):
+    '''Execute all of the store specific commands '''
+    if len(argv)>1:
+
+        if argv[1]=='save':
+            print(save(argv[2],argv[3]))
+
+        elif argv[1]=='recall':
+            print(recall(argv[2]))
+
+        elif argv[1]=='test':
+            from store_test import store_checker
+            store_checker()
--- bin/store_test.py
+++ bin/store_test.py
@@ -1,40 +1,57 @@
 from time import sleep
 
 from store import save, recall, expire, expiration
+from tst import run_diff_checks
 
 
 # Save and restore values
-def redis_test():
-    #assert(recall('test_redis/name')=='Eric')
-    assert(save  ('test_redis/name','Mark')==None)
-    assert(recall('test_redis/name')=='Mark')
-    assert(save  ('test_redis/name','Eric')==None)
-    assert(recall('test_redis/name')=='Eric')
+def store_redis_test():
+    lines = [
+        recall('test_redis/name'),
+        save  ('test_redis/name','Mark'),
+        recall('test_redis/name'),
+        save  ('test_redis/name','Eric'),
+        recall('test_redis/name')
+    ]
+    return '\n'.join(lines)
 
 
 # Set and check the expiration dates
-def redis_expiration_test():
-    save  ('test_redis/expiration','Yeah')
-    save  ('test_redis/expiration1','Yeah')
-    expire('test_redis/expiration1',1)
-    expire('test_redis/expiration',10)
-    assert(expiration('test_redis/expiration')==10)
-    assert(expiration('test_redis/expiration1')==1)
-    assert(recall('test_redis/expiration1')=='Yeah')
-    sleep(1)    
-    assert(expiration('test_redis/expiration')==9)
-    assert(not recall('test_redis/expiration1'))
+def store_expiration_test():
+    save  ('test_redis/expiration','Yeah'),
+    save  ('test_redis/expiration1','Yeah'),
+    expire('test_redis/expiration1',1),
+    expire('test_redis/expiration',10),
+    lines = [
+        str(expiration('test_redis/expiration')),
+        str(expiration('test_redis/expiration1')),
+        str(recall('test_redis/expiration1')),
+        str(sleep(1)),
+        str(expiration('test_redis/expiration')),
+        str(recall('test_redis/expiration1')),
+    ]
+    return '\n'.join(lines)
 
 
 # Get missing values
-def redis_missing_test():
-    assert(recall('test_redis/missing')==None)
-    assert(recall('test_redis/name')!=None)
+def store_missing_test():
+    lines = [
+        str(recall('test_redis/missing')),
+        str(recall('test_redis/name')!=None)
+    ]
+    return '\n'.join(lines)
 
 
-# Run all tests
-if __name__ == '__main__':
-    print('Testing Store for Redis Variables')
-    redis_test()
-    redis_expiration_test()
-    redis_missing_test()
+def store_checker():
+    '''Execute all the desired diff tests'''
+    my_tests = {
+        'store-expiration': store_expiration_test,
+        'store-missing': store_missing_test,
+        'store-redis': store_redis_test,
+    }
+    run_diff_checks('store', my_tests)
+
+
+# Create a script that can be run from the tst
+if __name__=='__main__':
+    store_checker()
